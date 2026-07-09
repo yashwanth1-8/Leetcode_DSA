77a77b6600a23a2a29f656dc68f52402b6c75804
Avula Yashwanth <avulayashwanth18@gmail.com>
2026-07-09 14:14:17 +0530

    Time: 16 ms (80.41%), Space: 79.3 MB (96.48%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,29 @@
+
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+
+                int width;
+                if (st.empty()) {
+                    width = i;
+                } else {
+                    width = i - st.top() - 1;
+                }
+
+                maxArea = max(maxArea, height * width);
+            }
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};
