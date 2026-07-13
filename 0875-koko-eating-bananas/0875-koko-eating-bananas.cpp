b1d3bb3b4f94a86d3c545c74e9376752ae35d393
Avula Yashwanth <avulayashwanth18@gmail.com>
2026-07-13 14:55:31 +0530

    Time: 7 ms (70.48%), Space: 23 MB (14.81%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    bool canFinish(const vector<int>& piles, int h, int k) {
-        long long hours = 0;
-        for (int pile : piles) {
-            hours += pile / k;
-            if (pile % k != 0) hours++;
-            if (hours > h) return false;
-        }
-        return hours <= h;
-    }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1;
-        int right = *max_element(piles.begin(), piles.end());
-        int ans = right;
+        int high=INT_MIN, mid;
+        int low=1;
+        for(int i=0;i<piles.size();i++){
+            high=max(high,piles[i]);
+        }
+        int ans=0;
+        while(low<=high){
+            mid= low+ (high-low)/2;
+            long long hr=0;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            for(int i=0;i<piles.size();i++){
+                int x= piles[i]/mid;
+                int r= piles[i]%mid;
+                if(r==0){
+                    hr=hr+x;
+                }
+                else{
+                hr= hr+x+1;
+                }
+            }
+            if(hr<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else if(hr>h){
+                low=mid+1;
             }
         }
+
         return ans;
     }
 };
